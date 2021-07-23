c992872830fa08eb7ec58ea169442110429fc166
ErrorInever <errorinever@gmail.com>
2021-07-23 12:58:58 +0000

    fix dev

--- metric_logger.py
+++ metric_logger.py
@@ -35,7 +35,7 @@ class MetricLogger:
     def log_image(self, fixed_y, fixed_fake_y, epoch, batch_idx, normalize=True):
         fixed_fake_y_grid = torchvision.utils.make_grid(fixed_fake_y, nrow=1, normalize=normalize, scale_each=True)
         fixed_y_grid = torchvision.utils.make_grid(fixed_y, nrow=1, normalize=normalize, scale_each=True)
-        fixed_image = torch.cat((fixed_y_grid, fixed_fake_y_grid), dim=2)
+        fixed_image = torch.cat((fixed_y_grid.cpu(), fixed_fake_y_grid.cpu()), dim=2)
         wandb.log({'fixed_y': [wandb.Image(np.moveaxis(fixed_image.detach().cpu().numpy(), 0, -1))]})
         img_name = f"epoch{epoch}_step_{batch_idx}.jpg"
         self.save_torch_images(fixed_image, img_name)
